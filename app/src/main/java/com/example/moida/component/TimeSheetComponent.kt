27053394a81c831731242c1ade302380cbf8a73d
chrin05 <rinring105@gmail.com>
2024-06-13 11:33:27 +0900

    feat : 일자 변경 및 우측 숫자컬러 구성

--- app/src/main/java/com/example/moida/component/TimeSheetComponent.kt
+++ app/src/main/java/com/example/moida/component/TimeSheetComponent.kt
@@ -1,7 +1,9 @@
 package com.example.moida.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -9,7 +11,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -18,9 +22,48 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.moida.R
 import com.example.moida.ui.theme.Pretendard
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
-fun DateOfTimeSheet(
+fun HeadOfTime( //날짜+요일 3개 출력
+    startDate: String,
+    startDay: DayOfWeek,
+    page: Int
+) {
+    val dateFormatter = DateTimeFormatter.ofPattern("MM.dd")
+    var currentDate = LocalDate.parse(startDate, DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+    var currentDay = startDay
+
+    Column(
+        modifier = Modifier
+    ) {
+        Row(
+        ) {
+            if (page==3) {
+                DateOfTimeSheet(
+                    date = currentDate.format(dateFormatter),
+                    day = currentDay.getDisplayName(java.time.format.TextStyle.FULL, Locale.KOREAN)
+                )
+            } else {
+                for (i in 0 until 3) {
+                    DateOfTimeSheet(
+                        date = currentDate.format(dateFormatter),
+                        day = currentDay.getDisplayName(java.time.format.TextStyle.FULL, Locale.KOREAN)
+                    )
+
+                    currentDate = currentDate.plusDays(1)
+                    currentDay = currentDay.plus(1)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DateOfTimeSheet( //날짜+요일
     date: String,
     day: String
 ) {
@@ -44,16 +87,51 @@ fun DateOfTimeSheet(
 }
 
 @Composable
-fun NumberBlock(
+fun NumberSection(participants: Int) { //숫자블록전체
+    if (participants < 4) {
+        var end = 2 //기본으로 0,1,2
+        if (participants == 3) end = 3 // 3명인 경우 0,1,2,3
+        for (i in 0..end) {
+            NumberBlock(peopleRange = i.toString(), fillColor = i)
+        }
+    } else {
+        var startNum = 1
+        var endNum = participants / 3
+        NumberBlock(peopleRange = "0", fillColor = 0)
+
+        for (i in 1..3) {
+            if (startNum == endNum) NumberBlock(peopleRange = "$startNum", fillColor = i)
+            else NumberBlock(peopleRange = "$startNum-$endNum", fillColor = i)
+
+            startNum = endNum + 1
+            if (i == 2) endNum = participants
+            else endNum *= 2
+        }
+    }
+}
+
+@Composable
+fun NumberBlock( //숫자 블록 하나
     peopleRange: String,
     fillColor: Int
 ) {
-    Row {
+    val backgroundColor: Int
+
+    if (fillColor == 0) backgroundColor = R.color.white
+    else if (fillColor == 1) backgroundColor = R.color.blue3
+    else if (fillColor == 2) backgroundColor = R.color.blue1
+    else backgroundColor = R.color.main_blue
+
+    Row(
+        modifier = Modifier,
+        verticalAlignment = Alignment.Bottom
+    ) {
         Box(
             modifier = Modifier
                 .width(24.dp)
                 .height(33.dp)
-                .background(color = colorResource(id = fillColor))
+                .background(color = colorResource(id = backgroundColor))
+                .border(width = 1.dp, color = colorResource(id = R.color.gray_100))
         )
         Spacer(modifier = Modifier.padding(6.dp))
         Text(
--- app/src/main/java/com/example/moida/screen/TimeSheet.kt
+++ app/src/main/java/com/example/moida/screen/TimeSheet.kt
@@ -2,7 +2,7 @@ package com.example.moida.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,16 +10,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.moida.R
 import com.example.moida.component.DateOfTimeSheet
+import com.example.moida.component.HeadOfTime
+import com.example.moida.component.NumberBlock
+import com.example.moida.component.NumberSection
 import com.example.moida.component.TitleWithXBtn
 import com.example.moida.model.Routes
 import java.time.DayOfWeek
@@ -33,63 +36,78 @@ fun TimeSheet(
     navController: NavHostController,
     title: String
 ) {
+    var page by remember { mutableIntStateOf(1) }
+    var startDate by remember { mutableStateOf("2024.04.04") }
+    var startDay by remember { mutableStateOf(getDayofWeek(startDate)) }
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+
     Column {
+        //제목부분
         TitleWithXBtn(
             navController = navController,
             route = Routes.CreateGroupSchedule.route,
             title = title,
             rightBtn = true
         )
-
+        //날짜부분
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 44.dp)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.chevron_left),
-                contentDescription = "왼쪽버튼",
-                modifier = Modifier
-                    .size(16.dp)
-                    .clickable {
-                    }
-            )
-            HeadOfTime(startDate = "2024.04.04", startDay = DayOfWeek.MONDAY.value)
-            Image(
-                painter = painterResource(id = R.drawable.chevron_right),
-                contentDescription = "오른쪽버튼",
-                modifier = Modifier
-                    .size(16.dp)
-                    .clickable {
-                    }
-            )
-        }
-    }
-}
-
-@Composable
-fun HeadOfTime(
-    startDate: String,
-    startDay: Int
-) {
-    val dateFormatter = DateTimeFormatter.ofPattern("MM.dd")
-    var currentDate = LocalDate.parse("2024.05.05", DateTimeFormatter.ofPattern("yyyy.MM.dd"))
-    var currentDay = DayOfWeek.MONDAY
-
-    Column(
-        modifier = Modifier
-    ) {
-        Row(
-        ) {
-            for (i in 0 until 3) {
-                DateOfTimeSheet(
-                    date = currentDate.format(dateFormatter),
-                    day = currentDay.getDisplayName(TextStyle.FULL, Locale.KOREAN)
+            if (page == 1) {
+                Box(modifier = Modifier.size(16.dp))
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.chevron_left),
+                    contentDescription = "왼쪽버튼",
+                    modifier = Modifier
+                        .size(16.dp)
+                        .clickable {
+                            page -= 1
+                            val date = LocalDate.parse(startDate, formatter)
+                            val newDate = date.minusDays(3)
+                            startDate = newDate.format(formatter)
+                            startDay = getDayofWeek(startDate)
+                        }
                 )
-
-                currentDate = currentDate.plusDays(1)
-                currentDay = currentDay.plus(1)
+            }
+            HeadOfTime(startDate = startDate, startDay = startDay, page = page)
+            if (page == 3) {
+                Box(modifier = Modifier.size(16.dp))
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.chevron_right),
+                    contentDescription = "오른쪽버튼",
+                    modifier = Modifier
+                        .size(16.dp)
+                        .clickable {
+                            page += 1
+                            val date = LocalDate.parse(startDate, formatter)
+                            val newDate = date.plusDays(3)
+                            startDate = newDate.format(formatter)
+                            startDay = getDayofWeek(startDate)
+                        }
+                )
             }
         }
+
+        
+
+
+        //우측 인원수 박스 : 모임인원수 넘겨줌 => 변경 필요
+        NumberSection(participants = 2)
     }
 }
+
+fun getDayofWeek(startDate: String): DayOfWeek {
+    // 날짜 문자열을 파싱하기 위한 포맷터 정의
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    // LocalDate 객체로 변환
+    val date = LocalDate.parse(startDate, formatter)
+
+    return date.dayOfWeek
+}
+
+
+
